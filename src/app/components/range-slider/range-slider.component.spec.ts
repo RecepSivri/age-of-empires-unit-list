70e6a55edc2999f28ab91b4d53857f952e5351d5
Recep Sivri <rsivri78@gmail.com>
2024-06-11 17:37:44 +0300

    code coverage updated over of %80

--- src/app/components/range-slider/range-slider.component.spec.ts
+++ src/app/components/range-slider/range-slider.component.spec.ts
@@ -22,4 +22,14 @@ describe('RangeSliderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('onChecked --test', () => {
+    component.onChecked(true)
+    expect(component.checked).toEqual(true)
+  });
+
+  it('changeValues --test', () => {
+    component.changeValues([10,20])
+    expect([component.min, component.max]).toEqual([10,20])
+  });
 });
--- src/app/components/table/page-table/page-table.component.spec.ts
+++ src/app/components/table/page-table/page-table.component.spec.ts
@@ -24,4 +24,110 @@ describe('PageTableComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('ngOnChanges --test', () => {
+    component.pagination =  {
+      size: 10,
+      current: 1,
+      pageSize: 10,
+      pageListSize: 5,
+      datas: [],
+      length: 0,
+      filteredDatas: []
+    }
+    component.ngOnChanges();
+    expect(component.listIndex).toEqual(0);
+  });
+
+  it('increase --test', () => {
+    component.pagination =  {
+      size: 10,
+      current: 1,
+      pageSize: 10,
+      pageListSize: 5,
+      datas: [],
+      length: 0,
+      filteredDatas: []
+    }
+    component.increase();
+    component.increase();
+    component.increase();
+    component.increase();
+    component.increase();
+    
+    expect(component.listIndex).toEqual(1);
+  });
+
+  it('increase --test', () => {
+    component.pagination =  {
+      size: 10,
+      current: 1,
+      pageSize: 10,
+      pageListSize: 5,
+      datas: [],
+      length: 0,
+      filteredDatas: []
+    }
+    for (let i = 0; i < 5; i++) {
+      component.increase();
+    }
+    
+    expect(component.listIndex).toEqual(1);
+  });
+
+  it('decrease --test', () => {
+    component.pagination =  {
+      size: 10,
+      current: 1,
+      pageSize: 10,
+      pageListSize: 5,
+      datas: [],
+      length: 0,
+      filteredDatas: []
+    }
+    for (let i = 0; i < 5; i++) {
+      component.increase();
+    }
+
+    component.decrease();
+    
+    expect(component.listIndex).toEqual(0);
+  });
+
+  it('setPage --test', () => {
+    component.setPage(5);
+    expect(component.current).toEqual(5);
+  });
+
+  it('returnPageListSize --test', () => {
+    component.pagination =  {
+      size: 11,
+      current: 1,
+      pageSize: 10,
+      pageListSize: 5,
+      datas: [],
+      length: 0,
+      filteredDatas: []
+    }
+    expect( component.returnPageListSize(2)).toEqual(5);
+  });
+
+  it('returnPageListSize --test', () => {
+    component.pagination =  null
+    expect( component.returnPageListSize(2)).toEqual(0);
+  });
+
+  it('setFinalPage --test', () => {
+    component.pagination =  {
+      size: 11,
+      current: 1,
+      pageSize: 10,
+      pageListSize: 5,
+      datas: [],
+      length: 0,
+      filteredDatas: []
+    }
+    component.setFinalPage(11)
+    expect( component.current).toEqual(11);
+  });
 });
