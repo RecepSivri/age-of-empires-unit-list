daf1dbb6f05bd526662638d3c7921f242d231469
Recep Sivri <rsivri78@gmail.com>
2024-06-11 17:57:32 +0300

    coverage updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,18 @@
-import { TestBed } from "@angular/core/testing";
+import { TestBed, fakeAsync, tick } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
 import { provideMockStore } from "@ngrx/store/testing";
+import { DetailComponent } from "./pages/detail/detail.component";
+import { Router } from "@angular/router";
 
 describe("AppComponent", () => {
+  let router: Router;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [RouterTestingModule, RouterTestingModule.withRoutes([
+        { path: 'units', component: DetailComponent }
+      ])],
+      declarations: [AppComponent, DetailComponent],
       providers: [provideMockStore({})],
     }).compileComponents();
   });
@@ -17,4 +22,15 @@ describe("AppComponent", () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it("app ngOnInit --test", fakeAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    router.initialNavigation();
+    app.ngOnInit();
+    router.navigate(['/units']);
+    tick();
+    expect(app.currentUrl).toEqual('/units');
+  }));
 });
